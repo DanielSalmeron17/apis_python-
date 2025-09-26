6ebc986a26b77dd1b8eaca11429c6bcffd912e10
DanielSalmeron17 <daniel_s@atdac.com.mx>
2025-09-26 09:09:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenido a la API Carlos"}
+    return {"message": "Bienvenido a la API Izac"}
 
 @app.get("/saludo")
 def saludo(nombre: Optional[str] = Query(default="Juan")):
